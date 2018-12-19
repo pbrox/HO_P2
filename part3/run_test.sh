135814857dab4c68e90b2e6c8fd151a5d7c5f44b
pbrox <pablobrox@gmail.com>
2018-12-19 01:18:19 +0100

    Fixed running scripts

--- part3/run_test.sh
+++ part3/run_test.sh
@@ -2,7 +2,7 @@
 rm -rf ../testmaps/*.output
 rm -rf ../testmaps/*.statistics
 for f in ../testmaps/*.txt; do
-        for i in min_manhattan_distance sum_manhattan_distance; do
+        for i in sum_diagonal_distance; do
                 echo "File $f, heuristic $i"
                 build/AstarPaganitzu $f $i
                 mv $f.output $f.$i.output
