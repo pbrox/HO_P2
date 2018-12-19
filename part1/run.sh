6abc421ebae26f8d2d17f02a3279a3445f94cea4
Jorge <jorgesm97@gmail.com>
2018-12-19 16:30:59 +0100

    More Maps

--- part1/run.sh
+++ part1/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-java -classpath .:jacop-4.3.0.jar SATPaganitzu /Users/jorgesanmartinlopez/Documents/GitHub/HO_P2/part1/maps/dosMapa.txt 10
+java -classpath .:jacop-4.3.0.jar SATPaganitzu maps/labWrongChar-part1.lab 7
 
